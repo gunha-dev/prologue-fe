ac88653cf6f8718acb4d5a2d8e8a9e170ef6c1a3
gunha <wh8299@gmail.com>
2025-08-21 09:50:15 +0900

    fe init

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <el-menu mode="horizontal">
+    <el-menu-item
+      v-for="menu in menus"
+      :key="menu.index"
+      @select="handleSelect"
+    >
+    {{ menu.menuName }}
+  </el-menu-item>
+  </el-menu>
+</template>
+
+<script>
+import data from "./data/data";
+
+export default {
+  name: "App",
+  data() {
+    return {
+      menus: data.getMenuData(),
+      curMenu: 0,
+    };
+  },
+  methods: {
+    handleSelect() {
+    },
+  },
+};
+</script>
+<style></style>
---
+++ src/data/data.js
@@ -0,0 +1,25 @@
+const data = {
+  getMenuData: () => {
+    const menu = [
+      {
+        index: 0,
+        menuName: "menu 1",
+      },
+      {
+        index: 1,
+        menuName: "menu 2",
+      },
+      {
+        index: 2,
+        menuName: "menu 3",
+      },
+      {
+        index: 3,
+        menuName: "ModalMenu",
+      },
+    ];
+    return menu;
+  },
+};
+
+export default data;
